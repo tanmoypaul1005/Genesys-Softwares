f9cd5eb58fee533b290d345e58d7ca0f15bdd2a8
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-16 17:27:19 +0600

    Refactor FutureProduct component and add new images

--- app/components/FutureProduct.js
+++ app/components/FutureProduct.js
@@ -1,4 +1,4 @@
-import { iMan1 } from '@/util/imageImports'
+import { iMan1, iMan2, iMan3, iMan4 } from '@/util/imageImports'
 import Image from 'next/image'
 import React from 'react'
 
@@ -6,7 +6,14 @@ const FutureProduct = () => {
     return (
         <div className='flex justify-between px-10 py-10'>
             <div className='text-white'>
-                <Image width={250} height={250} src={iMan1} alt="" />
+                <Image
+                 style={{
+                    maxHeight:250,
+                    minHeight:250,
+                    maxWidth:250,
+                    minWidth:250
+                 }}
+                 width={250} height={250} className='rounded-xl' src={iMan1} alt="" />
                 <div className='flex flex-col mt-4 gap-y-3'>
                 <div className='text-base font-medium leading-5 text-white'>Army Green Active Short Sleeve</div>
                 <div className='flex text-xl font-medium leading-6 gap-x-3'>
@@ -17,7 +24,12 @@ const FutureProduct = () => {
             </div>
 
             <div className='text-white'>
-                <Image width={250} height={250} src={iMan1} alt="" />
+                <Image style={{
+                    maxHeight:250,
+                    minHeight:250,
+                    maxWidth:250,
+                    minWidth:250
+                 }} width={250} className='rounded-xl' height={250} src={iMan2} alt="" />
                 <div className='flex flex-col mt-4 gap-y-3'>
                 <div className='text-base font-medium leading-5 text-white'>Army Green Active Short Sleeve</div>
                 <div className='flex text-xl font-medium leading-6 gap-x-3'>
@@ -29,7 +41,12 @@ const FutureProduct = () => {
 
 
             <div className='text-white'>
-                <Image width={250} height={250} src={iMan1} alt="" />
+                <Image style={{
+                    maxHeight:250,
+                    minHeight:250,
+                    maxWidth:250,
+                    minWidth:250
+                 }} width={250} className='rounded-xl' height={250} src={iMan3} alt="" />
                 <div className='flex flex-col mt-4 gap-y-3'>
                 <div className='text-base font-medium leading-5 text-white'>Army Green Active Short Sleeve</div>
                 <div className='flex text-xl font-medium leading-6 gap-x-3'>
@@ -41,7 +58,12 @@ const FutureProduct = () => {
 
 
             <div className='text-white'>
-                <Image width={250} height={250} src={iMan1} alt="" />
+                <Image  style={{
+                    maxHeight:250,
+                    minHeight:250,
+                    maxWidth:250,
+                    minWidth:250
+                 }} width={250} className='rounded-xl' height={250} src={iMan4} alt="" />
                 <div className='flex flex-col mt-4 gap-y-3'>
                 <div className='text-base font-medium leading-5 text-white'>Army Green Active Short Sleeve</div>
                 <div className='flex text-xl font-medium leading-6 gap-x-3'>
