e9315927e523f952676b0f5547e3dbf589657977
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-17 07:05:45 +0600

    Refactor Services component to adjust text width and add consistent font size

--- app/components/Services.js
+++ app/components/Services.js
@@ -8,25 +8,25 @@ const Services = () => {
             <div className='text-white'>
                 <Image src={iDelivery} alt=""/>
                 <div className='mt-6 mb-3 text-2xl font-medium leading-7'>Shipping</div>
-                <div className='text-base font-normal line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
+                <div className='text-base font-normal w-[250px] line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
             </div>
 
             <div className='text-white'>
                 <Image src={iExchange} alt=""/>
                 <div className='mt-6 mb-3 text-2xl font-medium leading-7'>Shipping</div>
-                <div className='text-base font-normal line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
+                <div className='text-base font-normal w-[250px] line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
             </div>
 
             <div className='text-white'>
                 <Image src={iCustomerSupport} alt=""/>
                 <div className='mt-6 mb-3 text-2xl font-medium leading-7'>Shipping</div>
-                <div className='text-base font-normal line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
+                <div className='text-base font-normal w-[250px] line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
             </div>
 
             <div className='text-white'>
                 <Image src={iVerified} alt=""/>
                 <div className='mt-6 mb-3 text-2xl font-medium leading-7'>Shipping</div>
-                <div className='text-base font-normal line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
+                <div className='text-base w-[250px] font-normal line-clamp-2'>The attention to detail is evident in every aspect of this backpack.</div>
             </div>
         </div>
     )
