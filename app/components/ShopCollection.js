e914f49d49bbe8e6794122a53463baeb96956a29
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-17 22:40:10 +0600

    Refactor image imports and component styles for ShopCollection

--- app/components/ShopCollection.js
+++ app/components/ShopCollection.js
@@ -10,7 +10,7 @@ const ShopCollection = () => {
         <div className='tracking-wide outlined-text'>Collection</div>
       </div>
 
-      <div className='flex items-end justify-between gap-x-10 '>
+      <div className='items-end justify-between hidden md:flex gap-x-10 '>
 
         <div className='relative w-full'>
           <Image style={{
@@ -28,28 +28,28 @@ const ShopCollection = () => {
 
         <div className='flex flex-col w-full gap-y-5'>
           <div className='relative'>
-          <Image
-            style={{
-              maxHeight: 180,
-              minHeight: 180,
-              maxWidth: "100%",
-              minWidth: "100%"
-            }}
-            src={iMultiCap} className='object-cover opacity-75' alt="" />
+            <Image
+              style={{
+                maxHeight: 180,
+                minHeight: 180,
+                maxWidth: "100%",
+                minWidth: "100%"
+              }}
+              src={iMultiCap} className='object-cover opacity-75' alt="" />
             <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>HEADW EAR</div>
           </div>
 
-          <div className='relative'> 
-          <Image
-            style={{
-              maxHeight: 180,
-              minHeight: 180,
-              maxWidth: "100%",
-              minWidth: "100%"
-            }}
-            className='opacity-75'
-            src={iMultiBook} alt="" />
-          <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>VALUE BOOKS</div>
+          <div className='relative'>
+            <Image
+              style={{
+                maxHeight: 180,
+                minHeight: 180,
+                maxWidth: "100%",
+                minWidth: "100%"
+              }}
+              className='opacity-75'
+              src={iMultiBook} alt="" />
+            <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>VALUE BOOKS</div>
           </div>
         </div>
 
@@ -67,32 +67,119 @@ const ShopCollection = () => {
 
         <div className='flex flex-col w-full gap-y-5'>
           <div className='relative'>
-          <Image
-            style={{
-              maxHeight: 250,
-              minHeight: 250,
-              maxWidth: "100%",
-              minWidth: "100%"
-            }}
-            className='opacity-75'
-            src={iTShirt1} alt="" 
+            <Image
+              style={{
+                maxHeight: 250,
+                minHeight: 250,
+                maxWidth: "100%",
+                minWidth: "100%"
+              }}
+              className='opacity-75'
+              src={iTShirt1} alt=""
             />
-<div className='absolute w-[30px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>The Future Looks Bright Collection</div>
+            <div className='absolute w-[30px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>The Future Looks Bright Collection</div>
           </div>
 
           <div className='relative'>
+            <Image
+              style={{
+                maxHeight: 250,
+                minHeight: 250,
+                maxWidth: "100%",
+                minWidth: "100%"
+              }}
+              className='opacity-75'
+              src={iTShirt2} alt="" />
+            <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>TOPS</div>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className='flex flex-col items-center justify-center md:hidden gap-y-10 '>
+
+        <div className='relative'>
           <Image
             style={{
               maxHeight: 250,
               minHeight: 250,
-              maxWidth: "100%",
-              minWidth: "100%"
+              maxWidth: 250,
+              minWidth: 250
+            }}
+            src={iBeg1}
+            alt=""
+            className='opacity-75'
+          />
+          <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>THE BOSS COLLECTION</div>
+        </div>
+
+
+        <div className='relative'>
+          <Image
+            style={{
+              maxHeight: 250,
+              minHeight: 250,
+              maxWidth: 250,
+              minWidth: 250
+            }}
+            src={iMultiCap} className='object-cover opacity-75' alt="" />
+          <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>HEADW EAR</div>
+        </div>
+
+        <div className='relative'>
+          <Image
+            style={{
+              maxHeight: 250,
+              minHeight: 250,
+              maxWidth: 250,
+              minWidth: 250
+            }}
+            className='opacity-75'
+            src={iMultiBook} alt="" />
+          <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>VALUE BOOKS</div>
+        </div>
+
+
+        <div className='relative '>
+          <Image
+            style={{
+              maxHeight: 250,
+              minHeight: 250,
+              maxWidth: 250,
+              minWidth: 250
+            }}
+            className='object-cover opacity-75'
+            src={iWaterBottle} alt="" />
+          <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>Accessories</div>
+        </div>
+
+
+        <div className='relative'>
+          <Image
+            style={{
+              maxHeight: 250,
+              minHeight: 250,
+              maxWidth: 250,
+              minWidth: 250
+            }}
+            className='opacity-75'
+            src={iTShirt1} alt=""
+          />
+          <div className='absolute w-[30px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>The Future Looks Bright Collection</div>
+        </div>
+
+        <div className='relative'>
+          <Image
+            style={{
+              maxHeight: 250,
+              minHeight: 250,
+              maxWidth: 250,
+              minWidth: 250
             }}
             className='opacity-75'
             src={iTShirt2} alt="" />
-            <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>TOPS</div>
-          </div>
-        
+          <div className='absolute w-[150px] bottom-4 left-4 text-white font-fold text-[32px] leading-[38px]'>TOPS</div>
         </div>
       </div>
     </div>
@@ -101,21 +188,4 @@ const ShopCollection = () => {
 
 export default ShopCollection;
 
-
-
-const CollectionItem = ({ title }) => {
-  return (
-    <div className="p-5 transition bg-gray-800 rounded-lg hover:bg-gray-700">
-      <div className="h-64 mb-4 bg-gray-600 rounded">
-        {/* Dummy image placeholder */}
-        <img
-          src="https://via.placeholder.com/300x200"
-          alt={title}
-          className="object-cover w-full h-full rounded"
-        />
-      </div>
-      <h3 className="text-xl font-semibold text-white">{title}</h3>
-    </div>
-  );
-};
 
