2614e0e3c5611fc8bc997f217a40387a50f5a4a8
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-17 10:24:30 +0600

    Refactor Header component to adjust image sizes and improve responsiveness

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,7 +6,7 @@ import FutureProduct from './FutureProduct'
 const Header = () => {
     return (
         <div className='bg-[#EA2127] pb-[60px] px-[100px]'>
-            <div className=' text-black py-[50px]'>
+            <div className='text-black py-5  md:py-[50px]'>
                 <div className='flex justify-between'>
 
                     <div className='flex gap-x-2'>
@@ -22,7 +22,7 @@ const Header = () => {
                     <Image height={900} width={900}  src={iHeaderSign} alt="" />
                 </div>
 
-                <div className='mt-[-40px]'>
+                <div className='mt-[-14px] md:mt-[-40px]'>
                 <FutureProduct />
                 </div>
             </div>
